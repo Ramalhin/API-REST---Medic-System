954cc8ec33a00b7d9278e759bb9e23c465e1756a
Ramalhin <j.ramalho@live.com>
2024-11-30 21:04:51 -0300

    Commit a3

---
+++ src/main/java/com/clinica/demo/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.clinica.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Altere para a URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
